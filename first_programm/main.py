061e9421a51f4825faf6f8ad9493c5946279cb89
Anton Fedosov <fedosov-toxa@mail.ru>
2023-12-13 11:48:52 +0300

    message box + working restart

--- first_programm/main.py
+++ first_programm/main.py
@@ -17,47 +17,55 @@ class MyThread(QtCore.QThread):
         QtCore.QThread.__init__(self, parent)
         self.time_format = None
         self.curr_answers = None
+        self.stop_word = None
 
     def run(self):
         init_time = 0
         while True:
-            self.sleep(1)
-            init_time += 1
-            self.time_format = time.strftime("%H:%M:%S", time.gmtime(init_time))
-            result_time[0] = str(init_time)
-            self.mysignal.emit("%s" % self.time_format)
+            if self.stop_word == 'stop':
+                break
+            else:
+                self.sleep(1)
+                init_time += 1
+                self.time_format = time.strftime("%H:%M:%S", time.gmtime(init_time))
+                result_time[0] = str(init_time)
+                self.mysignal.emit("%s" % self.time_format)
 
 
 class FirstTestWindow(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
+        self.my_thread = None
         self.ui_first_test = Ui_Form()
         self.ui_first_test.setupUi(self)
-        self.mythread = MyThread()
-        self.mythread.started.connect(self.on_started)
-        self.mythread.finished.connect(self.on_finished)
-        self.mythread.mysignal.connect(self.on_change, QtCore.Qt.QueuedConnection)
         self.button_start = self.ui_first_test.pushButton_start
         self.button_stop = self.ui_first_test.pushButton_stop
         self.button_start.clicked.connect(self.on_clicked)
         self.button_stop.clicked.connect(self.on_finished)
+        self.button_stop.setEnabled(False)
 
     def on_clicked(self):
-        self.mythread.start()
+        self.my_thread = MyThread()
+        self.my_thread.started.connect(self.on_started)
+        self.my_thread.finished.connect(self.on_finished)
+        self.my_thread.mysignal.connect(self.on_change, QtCore.Qt.QueuedConnection)
+        self.my_thread.start()
 
     def on_started(self):
         self.ui_first_test.label_timer.setText("")
+        self.button_stop.setEnabled(True)
 
     def on_finished(self):
-        QMessageBox.about(self, 'Ваш результат:', 'Тест завершен.\n{0} верных ответов из 10.'.format(
-            self.right_answer
-        ))
-        exit()
+        self.my_thread.stop_word = 'stop'
+        self.button_stop.setEnabled(False)
 
     def on_change(self, s):
         self.ui_first_test.label_timer.setText(s)
-
-
+        if self.my_thread.stop_word == 'stop':
+            msg_box = QMessageBox()
+            msg_box.setText("The document has been modified.")
+            msg_box.setWindowTitle("Внимание!")
+            msg_box.exec_()
 
 
 if __name__ == '__main__':
