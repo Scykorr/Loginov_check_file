aefef75709562dd087349e05c5ec7f12525964e8
Anton Fedosov <fedosov-toxa@mail.ru>
2023-12-13 12:58:38 +0300

    add choose folder

--- first_programm/main.py
+++ first_programm/main.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtCore
 
 from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMessageBox, QFileDialog
 from PyQt5.QtGui import QPixmap
 import sys
 from random import shuffle
@@ -38,11 +38,13 @@ class FirstTestWindow(QtWidgets.QWidget):
         self.my_thread = None
         self.ui_first_test = Ui_Form()
         self.ui_first_test.setupUi(self)
-        self.button_start = self.ui_first_test.pushButton_start
+        self.ui_first_test.pushButton_start.clicked.connect(self.on_clicked)
         self.button_stop = self.ui_first_test.pushButton_stop
-        self.button_start.clicked.connect(self.on_clicked)
         self.button_stop.clicked.connect(self.on_finished)
         self.button_stop.setEnabled(False)
+        self.ui_first_test.pushButton_files_folder.clicked.connect(self.choose_folder_files)
+        self.ui_first_test.pushButton_diode.clicked.connect(self.choose_folder_diode)
+
 
     def on_clicked(self):
         self.my_thread = MyThread()
@@ -54,19 +56,30 @@ class FirstTestWindow(QtWidgets.QWidget):
     def on_started(self):
         self.ui_first_test.label_timer.setText("")
         self.button_stop.setEnabled(True)
+        self.ui_first_test.pushButton_files_folder.setEnabled(False)
+        self.ui_first_test.pushButton_diode.setEnabled(False)
 
     def on_finished(self):
         self.my_thread.stop_word = 'stop'
         self.button_stop.setEnabled(False)
+        self.ui_first_test.pushButton_files_folder.setEnabled(True)
+        self.ui_first_test.pushButton_diode.setEnabled(True)
 
     def on_change(self, s):
         self.ui_first_test.label_timer.setText(s)
         if self.my_thread.stop_word == 'stop':
             msg_box = QMessageBox()
-            msg_box.setText("The document has been modified.")
+            msg_box.setText("Процесс завершен.")
             msg_box.setWindowTitle("Внимание!")
             msg_box.exec_()
 
+    def choose_folder_files(self):
+        dirlist = QFileDialog.getExistingDirectory(self, "Выбрать папку", ".")
+        self.ui_first_test.lineEdit_files_folder.setText(dirlist)
+
+    def choose_folder_diode(self):
+        dirlist = QFileDialog.getExistingDirectory(self, "Выбрать папку", ".")
+        self.ui_first_test.lineEdit_for_diode.setText(dirlist)
 
 if __name__ == '__main__':
     user_name = []
